c6ef69b792b8656ffa69bc37c8b86d31e7480f88
Nara-2906 <111081647+Nara-2906@users.noreply.github.com>
2023-01-15 20:16:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-jr4k6f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-jr4k6f)
---
+++ index.js
@@ -0,0 +1,5 @@
+const saldoNara = 1000;
+const saldoEafael = 10000;
+
+const total = saldoNara + saldoRafael;
+console.log(total);
